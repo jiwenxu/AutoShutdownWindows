84a8a56056db853d52974dcecbe95d84f9579e3a
xjw <150056336@qq.com>
2024-09-14 15:16:58 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace AutoShutdown {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ShowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.comboBoxHour = new System.Windows.Forms.ComboBox();
+            this.comboBoxMinute = new System.Windows.Forms.ComboBox();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "定时";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(269, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "启动目录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(269, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "初始化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ShowToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.ExitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(130, 54);
+            // 
+            // ShowToolStripMenuItem
+            // 
+            this.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem";
+            this.ShowToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.ShowToolStripMenuItem.Text = "显示/隐藏";
+            this.ShowToolStripMenuItem.Click += new System.EventHandler(this.ShowToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(126, 6);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.ExitToolStripMenuItem.Text = "退出";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "下次关机时间";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 30);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            // 
+            // comboBoxHour
+            // 
+            this.comboBoxHour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxHour.FormattingEnabled = true;
+            this.comboBoxHour.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.comboBoxHour.Location = new System.Drawing.Point(13, 223);
+            this.comboBoxHour.Name = "comboBoxHour";
+            this.comboBoxHour.Size = new System.Drawing.Size(94, 20);
+            this.comboBoxHour.TabIndex = 9;
+            // 
+            // comboBoxMinute
+            // 
+            this.comboBoxMinute.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMinute.FormattingEnabled = true;
+            this.comboBoxMinute.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBoxMinute.Location = new System.Drawing.Point(133, 222);
+            this.comboBoxMinute.Name = "comboBoxMinute";
+            this.comboBoxMinute.Size = new System.Drawing.Size(99, 20);
+            this.comboBoxMinute.TabIndex = 10;
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(269, 95);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(75, 21);
+            this.textBoxYear.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(269, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "添加";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(269, 186);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "删除";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(219, 21);
+            this.textBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 294);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.comboBoxMinute);
+            this.Controls.Add(this.comboBoxHour);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "定时关机";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ShowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ComboBox comboBoxHour;
+        private System.Windows.Forms.ComboBox comboBoxMinute;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoShutdown {
+    public partial class Form1 : Form {
+
+        private Dictionary<string, List<string>> _holidays = new Dictionary<string, List<string>>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e) {
+            this.Dispose();
+            Application.Exit();
+        }
+
+        private void ShowToolStripMenuItem_Click(object sender, EventArgs e) {
+            this.Visible = !this.Visible;
+            if (this.Visible) {
+                this.WindowState = FormWindowState.Normal;
+            } else {
+                this.WindowState = FormWindowState.Minimized;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.notifyIcon1.Icon = new Icon(@"window-close.ico");
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Text = this.Text;
+            this.comboBoxHour.SelectedIndex = 23;
+            this.comboBoxMinute.SelectedIndex = 0;
+            this.textBoxYear.Text = DateTime.Now.Year.ToString();
+            this.readHoliday();
+            this.selectNextShutdownDay();
+            this.monthCalendar1.MinDate = DateTime.Now;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            e.Cancel = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+        }
+
+        private void readHoliday() {
+            if (!File.Exists("holiday.json")) {
+                return;
+            }
+            string json = File.ReadAllText("holiday.json");
+            this._holidays = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(json);
+            this.setBoldedDates();
+        }
+
+        private void selectNextShutdownDay() {
+            DateTime start = DateTime.Now;
+            List<DateTime> boldedDates = new List<DateTime>();
+            foreach (string key in this._holidays.Keys) {
+                if(int.Parse(key) < start.Year) {
+                    continue;
+                }
+
+                foreach (string date in _holidays[key]) {
+                    DateTime tmp = DateTime.Parse(date);
+                    if (tmp < start) {
+                        continue;
+                    }
+                    boldedDates.Add(tmp);
+                }
+            }
+            
+            for(int i = 0; i < boldedDates.Count - 1; i++) {
+                TimeSpan ts = boldedDates[i + 1] - boldedDates[i];
+                if (ts.Days > 1) {
+                    this.monthCalendar1.SelectionStart = boldedDates[i];
+                    break;
+                }
+            }
+        }
+
+        private void writeHoliday(string json) {
+            try {
+                StreamWriter sw = new StreamWriter("holiday.json");
+                sw.WriteLine(json);
+                sw.Close();
+            } catch (Exception exception) {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private void setBoldedDates() {
+            List<DateTime> boldedDates = new List<DateTime>();
+            foreach (string key in this._holidays.Keys) {
+                foreach (string date in _holidays[key]) {
+                    boldedDates.Add(DateTime.Parse(date));
+                }
+            }
+            this.monthCalendar1.BoldedDates = boldedDates.ToArray();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Right) {
+                this.contextMenuStrip1.Show();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            // 启动文件夹
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            System.Diagnostics.Process.Start(path);
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            // 更新
+            string key = this.textBoxYear.Text;
+            if (_holidays.ContainsKey(key)) {
+                MessageBox.Show("已初始化该年份");
+                return;
+            }
+            DateTime start = DateTime.Parse(key + "-1-1");
+            List<string> days = new List<string>();
+            while (true) {
+                start = start.AddDays(1);
+                if (start.DayOfWeek == DayOfWeek.Saturday || start.DayOfWeek == DayOfWeek.Sunday) {
+                    days.Add(start.ToString("yyyy-MM-dd"));
+                }
+                if (start.Year.ToString() != key) {
+                    break;
+                }
+            }
+            this._holidays.Add(key, days);
+            string json = JsonConvert.SerializeObject(this._holidays);
+            writeHoliday(json);
+            this.setBoldedDates();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //设置关机时间
+            DateTime next = DateTime.Parse(this.monthCalendar1.SelectionStart.ToString("yyyy-MM-dd ") + this.comboBoxHour.Text + ":" + this.comboBoxMinute.Text);
+            TimeSpan ts = next - DateTime.Now;
+            string p = "shutdown /a&shutdown /s /f /t " + (int)ts.TotalSeconds;
+            Console.WriteLine(p);
+            //this.Exec("shutdown /a");
+            this.Exec(p);
+            this.textBox1.Text = p;
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            //添加一个休息日
+            DateTime date = this.monthCalendar1.SelectionStart;
+            string key = date.Year.ToString();
+            if (!this._holidays.ContainsKey(key)) {
+                MessageBox.Show($"请先始化{key}年");
+                return;
+            }
+            string val = date.ToString("yyyy-MM-dd");
+            if (this._holidays[key].Contains(val)) {
+                return;
+            }
+            this._holidays[key].Add(val);
+            this._holidays[key].Sort();
+            this.writeHoliday(JsonConvert.SerializeObject(this._holidays));
+            this.setBoldedDates();
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            // 删除一个休息日
+            DateTime date = this.monthCalendar1.SelectionStart;
+            string key = date.Year.ToString();
+            if (!this._holidays.ContainsKey(key)) {
+                MessageBox.Show($"请先始化{key}年");
+                return;
+            }
+            string val = date.ToString("yyyy-MM-dd");
+            if (!this._holidays[key].Contains(val)) {
+                return;
+            }
+            this._holidays[key].Remove(val);
+            this._holidays[key].Sort();
+            this.writeHoliday(JsonConvert.SerializeObject(this._holidays));
+            this.setBoldedDates();
+        }
+
+        public void Exec(string str) {
+            try {
+                using (Process process = new Process()) {
+                    process.StartInfo.FileName = "cmd.exe";//调用cmd.exe程序
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardInput = true;//重定向标准输入
+                    process.StartInfo.RedirectStandardOutput = true;//重定向标准输出
+                    process.StartInfo.RedirectStandardError = true;//重定向标准出错
+                    process.StartInfo.CreateNoWindow = true;//不显示黑窗口
+                    process.Start();//开始调用执行
+                    process.StandardInput.WriteLine(str + "&exit");//标准输入str + "&exit"，相等于在cmd黑窗口输入str + "&exit"
+                    process.StandardInput.AutoFlush = true;//刷新缓冲流，执行缓冲区的命令，相当于输入命令之后回车执行
+                    process.WaitForExit();//等待退出
+                    process.Close();//关闭进程
+                    MessageBox.Show("成功");
+                }
+            } catch {
+                MessageBox.Show("失败");
+            }
+        }
+
+    }
+}
